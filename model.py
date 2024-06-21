9d5f3da9e7f76d229f8fae1a48104b0f74ae774b
Versha111 <160007239+Versha111@users.noreply.github.com>
2024-06-21 13:26:57 +0530

    Update model.py

--- model.py
+++ model.py
@@ -70,7 +70,7 @@ async def start():
     chain = qa_bot()
     msg = cl.Message(content="Starting the bot...")
     await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    msg.content = "Hi, Welcome to Llama2 Chatbot. What is your query?"
     await msg.update()
 
     cl.user_session.set("chain", chain)
